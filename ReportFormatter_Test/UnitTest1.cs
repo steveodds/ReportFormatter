e55e0c541e2095f39d5948f7c5c8595836e181e4
Stephen Odundo <steveremdi@gmail.com>
2021-06-09 18:45:48 +0300

    added unit test wih sample order numbers and report file

--- ReportFormatter_Test/UnitTest1.cs
+++ ReportFormatter_Test/UnitTest1.cs
@@ -1,14 +1,31 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+using System.Collections.Generic;
 
 namespace ReportFormatter_Test
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        private string reportFile = @"C:\Users\user\Downloads\COLGATE.xlsx";
+        private List<string> orderNumbers = new List<string>()
         {
+            "NAR-1-CPEAL-01361-MRO-TV-011555",
+            "NAR-1-CPEAL-01333-MRO-TV-011111",
+            "NAR-1-CPEAL-01364-MRO-TV-011999",
+            "NAR-1-CPEAL-01366-MRO-TV-011000",
+            "NAR-1-CPEAL-01361-MRO-TV-011267",
+            "NAR-1-CPEAL-01361-MRO-TV-011954",
+            "NAR-1-CPEAL-01367-MRO-TV-011467",
+            "NAR-1-CPEAL-00000-MRO-TV-011224"
+        };
+
+
+        [TestMethod]
+        public void MainTest()
+        {
+            var formatter = new ReportFormatter.ReportFormatter();
+            var result = formatter.FinalizeReport(reportFile, orderNumbers);
+            Assert.IsTrue(result);
         }
     }
 }
